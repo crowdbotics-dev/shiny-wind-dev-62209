b72278577abfaabb56cceadf8a40ce4f27d62639
Crowdbotics <engineering@crowdbotics.com>
2023-02-09 00:21:36 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/shiny_wind_dev_62209/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shiny_wind_dev_62209;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "shiny_wind_dev_62209";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shiny-wind-dev-62209",
+  url: "https://shiny-wind-dev-62209.botics.co",
+  api: "https://shiny-wind-dev-62209.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
